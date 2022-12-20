86e51aa35a5f1ed79213dc9e10255166a5800584
Jan Wijffels <jwijffels@bnosac.be>
2022-12-20 23:04:45 +0100

    #9 CI

--- inst/tinytest/test_download.R
+++ inst/tinytest/test_download.R
@@ -1,14 +1,14 @@
 library(audio.whisper)
 
-## Download a model
-path  <- whisper_download_model("tiny", overwrite = FALSE)
-expect_inherits(path, "data.frame")
-expect_false(path$download_failed)
-
-## Load the model
-model <- whisper("tiny")
-expect_inherits(model, class = "whisper")
-expect_inherits(model$model, class = "externalptr")
-expect_false(identical(model$model, new("externalptr")))
-
-
+if(Sys.getenv("TINYTEST_CI", unset = "yes") == "yes"){
+  ## Download a model
+  path  <- whisper_download_model("tiny", overwrite = FALSE)
+  expect_inherits(path, "data.frame")
+  expect_false(path$download_failed)
+  
+  ## Load the model
+  model <- whisper("tiny")
+  expect_inherits(model, class = "whisper")
+  expect_inherits(model$model, class = "externalptr")
+  expect_false(identical(model$model, new("externalptr")))
+}
--- inst/tinytest/test_predict.R
+++ inst/tinytest/test_predict.R
@@ -2,7 +2,7 @@ library(audio.whisper)
 audio <- system.file(package = "audio.whisper", "samples", "jfk.wav")
 
 ## Load the model, predict a small fragment which does not detect anything
-model <- whisper("tiny")
+model <- whisper(system.file(package = "audio.whisper", "models", "for-tests-ggml-tiny.bin"))
 trans <- predict(model, newdata = audio, language = "en", duration = 1000 / 2)
 expect_inherits(trans, "whisper_transcription")
 expect_true(trans$n_segments == 0)
@@ -11,16 +11,18 @@ expect_true(nrow(trans$data) == 0)
 expect_true(is.data.frame(trans$tokens))
 expect_true(nrow(trans$tokens) == 0)
 
-## JFK example full fragment
-trans <- predict(model, newdata = audio, language = "en")
-expect_inherits(trans, "whisper_transcription")
-expect_true(trans$n_segments == 1)
-expect_true(is.data.frame(trans$data))
-expect_true(nrow(trans$data) == 1)
-expect_true(is.data.frame(trans$tokens))
-expect_true(nrow(trans$tokens) == 13)
-expect_equal(trimws(trans$data$text), "And so my fellow Americans ask not what your country can do for you ask what you can do for your country.")
-expect_equal(trimws(trans$tokens$token), c("And", "so", "my", "fellow", "Americans", "ask", "not", "what", 
-                                           "your", "country", "can", "do", "for", "you", "ask", "what", 
-                                           "you", "can", "do", "for", "your", "country", "."))
-
+if(Sys.getenv("TINYTEST_CI", unset = "yes") == "yes"){
+  ## JFK example full fragment using tiny model
+  model <- whisper("tiny")
+  trans <- predict(model, newdata = audio, language = "en")
+  expect_inherits(trans, "whisper_transcription")
+  expect_true(trans$n_segments == 1)
+  expect_true(is.data.frame(trans$data))
+  expect_true(nrow(trans$data) == 1)
+  expect_true(is.data.frame(trans$tokens))
+  expect_true(nrow(trans$tokens) == 13)
+  expect_equal(trimws(trans$data$text), "And so my fellow Americans ask not what your country can do for you ask what you can do for your country.")
+  expect_equal(trimws(trans$tokens$token), c("And", "so", "my", "fellow", "Americans", "ask", "not", "what", 
+                                             "your", "country", "can", "do", "for", "you", "ask", "what", 
+                                             "you", "can", "do", "for", "your", "country", "."))
+}
