3e14dfc33232e4318b7eb02ee374e0938bcf1ded
Jan Wijffels <jwijffels@bnosac.be>
2022-12-20 22:36:29 +0100

    #9 add unit tests

---
+++ inst/tinytest/test_predict.R
@@ -0,0 +1,26 @@
+library(audio.whisper)
+audio <- system.file(package = "audio.whisper", "samples", "jfk.wav")
+
+## Load the model, predict a small fragment which does not detect anything
+model <- whisper("tiny")
+trans <- predict(model, newdata = audio, language = "en", duration = 1000 / 2)
+expect_inherits(trans, "whisper_transcription")
+expect_true(trans$n_segments == 0)
+expect_true(is.data.frame(trans$data))
+expect_true(nrow(trans$data) == 0)
+expect_true(is.data.frame(trans$tokens))
+expect_true(nrow(trans$tokens) == 0)
+
+## JFK example full fragment
+trans <- predict(model, newdata = audio, language = "en")
+expect_inherits(trans, "whisper_transcription")
+expect_true(trans$n_segments == 1)
+expect_true(is.data.frame(trans$data))
+expect_true(nrow(trans$data) == 1)
+expect_true(is.data.frame(trans$tokens))
+expect_true(nrow(trans$tokens) == 13)
+expect_equal(trimws(trans$data$text), "And so my fellow Americans ask not what your country can do for you ask what you can do for your country.")
+expect_equal(trimws(trans$tokens$token), c("And", "so", "my", "fellow", "Americans", "ask", "not", "what", 
+                                           "your", "country", "can", "do", "for", "you", "ask", "what", 
+                                           "you", "can", "do", "for", "your", "country", "."))
+
