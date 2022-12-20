61af0052aa9d5d20075674c6823e51525c0c88a8
Jan Wijffels <jwijffels@bnosac.be>
2022-12-20 23:08:56 +0100

    #9 CI

--- inst/tinytest/test_predict.R
+++ inst/tinytest/test_predict.R
@@ -5,22 +5,22 @@ audio <- system.file(package = "audio.whisper", "samples", "jfk.wav")
 model <- whisper(system.file(package = "audio.whisper", "models", "for-tests-ggml-tiny.bin"))
 trans <- predict(model, newdata = audio, language = "en", duration = 1000 / 2)
 expect_inherits(trans, "whisper_transcription")
-expect_true(trans$n_segments == 0)
+expect_equal(trans$n_segments, 0)
 expect_true(is.data.frame(trans$data))
-expect_true(nrow(trans$data) == 0)
+expect_equal(nrow(trans$data), 0)
 expect_true(is.data.frame(trans$tokens))
-expect_true(nrow(trans$tokens) == 0)
+expect_equal(nrow(trans$tokens), 0)
 
 if(Sys.getenv("TINYTEST_CI", unset = "yes") == "yes"){
   ## JFK example full fragment using tiny model
   model <- whisper("tiny")
   trans <- predict(model, newdata = audio, language = "en")
   expect_inherits(trans, "whisper_transcription")
-  expect_true(trans$n_segments == 1)
+  expect_equal(trans$n_segments, 1)
   expect_true(is.data.frame(trans$data))
-  expect_true(nrow(trans$data) == 1)
+  expect_equal(nrow(trans$data), 1)
   expect_true(is.data.frame(trans$tokens))
-  expect_true(nrow(trans$tokens) == 13)
+  expect_equal(nrow(trans$tokens), 13)
   expect_equal(trimws(trans$data$text), "And so my fellow Americans ask not what your country can do for you ask what you can do for your country.")
   expect_equal(trimws(trans$tokens$token), c("And", "so", "my", "fellow", "Americans", "ask", "not", "what", 
                                              "your", "country", "can", "do", "for", "you", "ask", "what", 
