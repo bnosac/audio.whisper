eb98817b4edd47c530c91df79e1f8a093df181a2
Jan Wijffels <jwijffels@bnosac.be>
2024-03-04 23:55:38 +0100

    unit test multiple offsets

---
+++ inst/tinytest/test-multiple-offsets.R
@@ -0,0 +1,11 @@
+library(audio.whisper)
+
+if(Sys.getenv("TINYTEST_CI", unset = "yes") == "yes"){
+  ## JFK example full fragment using tiny model
+  model <- whisper("tiny")
+  trans <- predict(model, newdata = system.file(package = "audio.whisper", "samples", "jfk.wav"), language = "en", 
+                   offset = c(0, 4000), duration = c(1*1000, 1*2000))
+  expect_equal(trans$n_segments, 2)
+  expect_equal(nrow(trans$data), 2)
+  if(file.exists(model$file)) file.remove(model$file)
+}
