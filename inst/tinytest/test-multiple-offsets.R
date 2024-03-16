0483bf8f1cc726625666cb2693ad52994bf24bef
Jan Wijffels <jwijffels@bnosac.be>
2024-03-17 00:11:20 +0100

    unit test on longer file

--- inst/tinytest/test-multiple-offsets.R
+++ inst/tinytest/test-multiple-offsets.R
@@ -8,4 +8,14 @@ if(Sys.getenv("TINYTEST_CI", unset = "yes") == "yes"){
   expect_equal(trans$n_segments, 2)
   expect_equal(nrow(trans$data), 2)
   if(file.exists(model$file)) file.remove(model$file)
+  
+  ## Longer file
+  download.file("https://github.com/jwijffels/example/raw/main/example.wav", "example.wav")
+  model <- whisper("tiny")
+  trans <- predict(model, newdata = "example.wav", language = "en", 
+                   offset = c(7*1000, 60*1000), duration = c(4*1500, 2*1000))
+  expect_equal(trans$n_segments, 2)
+  expect_equal(nrow(trans$data), 2)
+  if(file.exists(model$file)) file.remove(model$file)
+  if(file.exists(trans$params$audio)) file.remove(trans$params$audio)
 }
