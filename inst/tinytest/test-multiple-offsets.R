75b4bdc5b1910440245aed0a6b2129d3f9182fec
Jan Wijffels <jwijffels@bnosac.be>
2024-03-17 23:25:41 +0100

    there is no guarantee on short segments that whisper.cpp adheres to the requested timeframe - it provides the whole segment - disable that unit test

--- inst/tinytest/test-multiple-offsets.R
+++ inst/tinytest/test-multiple-offsets.R
@@ -5,8 +5,8 @@ if(Sys.getenv("TINYTEST_CI", unset = "yes") == "yes"){
   model <- whisper("tiny")
   trans <- predict(model, newdata = system.file(package = "audio.whisper", "samples", "jfk.wav"), language = "en", 
                    offset = c(0, 4000), duration = c(1*1500, 1*5000))
-  expect_equal(trans$n_segments, 2)
-  expect_equal(nrow(trans$data), 2)
+  #expect_equal(trans$n_segments, 2)
+  #expect_equal(nrow(trans$data), 2)
   if(file.exists(model$file)) file.remove(model$file)
   
   ## Longer file
